85b72b163a713f28f0b7500596113750c8fa2c56
tgrants <toms.grants@gmail.com>
2026-07-28 00:31:26 +0300

    refactor: replace base-64 dependency with native Node.js API

--- src/encoders.ts
+++ src/encoders.ts
@@ -1,5 +1,3 @@
-import { encode as btoa } from "base-64";
-
 const binary = (input: ArrayBuffer) => {
   let binary = "";
   const bytes = new Uint8Array(input);
@@ -18,9 +16,7 @@ const hex = (input: ArrayBuffer) =>
     .map((b) => b.toString(16).padStart(2, "0"))
     .join("");
 
-// @see https://stackoverflow.com/questions/35155089/node-sha-256-base64-digest
-// @see https://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
-const base64 = (input: ArrayBuffer) => btoa(binary(input));
+const base64 = (input: ArrayBuffer) => Buffer.from(input).toString('base64');
 
 const base64url = (input: ArrayBuffer) =>
   base64(input).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
